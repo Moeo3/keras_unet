5a86a107ecb58e6710e8a3ea42a4146c5dcecd35
moeo3 <moeo3@hotmail.com>
2020-12-02 11:18:46 +0800

    copy

---
+++ model.py
@@ -0,0 +1,39 @@
+from keras.models import Input, Model
+from keras.layers.convolutional import Conv2D, Conv2DTranspose, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.backend import Concatenate
+from copy import deepcopy
+
+def conv_33(inputs, ch, acti):
+    outputs = Conv2D(ch, 3, activation=acti, padding='same')(inputs)
+    outputs = BatchNormalization()(outputs)
+    return outputs
+
+def conv_block(inputs, ch, acti):
+    outputs = conv_33(inputs, ch, acti)
+    outputs = conv_33(outputs, ch, acti)
+    return outputs
+
+def build_level(inputs, ch, depth, inc_rate, acti):
+    if depth == 0:
+        outputs = conv_block(inputs, ch, acti)
+    else:
+        # down
+        outputs = conv_block(inputs, ch, acti)
+        concatenate_part = deepcopy(outputs)
+        outputs = MaxPooling2D()(outputs)
+
+        #recursive
+        outputs = build_level(outputs, int(inc_rate * ch), depth - 1, inc_rate, acti)
+
+        #up
+        outputs = Conv2DTranspose(ch, 3, strides=2, activation=acti, padding='same')(outputs)
+        outputs = Concatenate()([concatenate_part, outputs])
+        outputs = conv_block(outputs, ch, acti)
+    return outputs
+
+def UNet(img_size = (512, 512, 1), start_conv_ch = 64, out_ch = 1, inc_rate = 2., acti = 'relu', max_depth = 4):
+    inputs = Input(img_size)
+    outputs = build_level(inputs, start_conv_ch, max_depth, inc_rate, acti)
+    outputs = Conv2D(out_ch, 1, activation='sigmoid')(outputs)
+    return Model(input=inputs, output=outputs)
